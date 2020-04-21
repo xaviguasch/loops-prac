a4011728d58bb18d20ca8c8aa7e6a41a69aae4f0
xaviguasch <xaviguasch@gmail.com>
2020-04-21 14:27:19 +0200

    added for of prac

--- app.js
+++ app.js
@@ -24,6 +24,31 @@ for (const key in user) {
 
 console.log('hey')
 
-// for (const val of Object.values(user)) {
-//   console.log(val);
-// }
+for (const val of Object.entries(user)) {
+  // entries prints an array with keys and values
+  console.log(val)
+
+  console.log(val[0])
+  console.log(val[1])
+}
+
+for (const val of Object.keys(user)) {
+  // keys prints an array with keys
+  console.log(val)
+}
+
+for (const val of Object.values(user)) {
+  // values prints an array with values
+  console.log(val)
+}
+
+const numbers = [1, 2, 3, 4, 5]
+
+for (const num of numbers) {
+  console.log(num)
+}
+
+for (let i = 0; i < numbers.length; i++) {
+  console.log(i)
+  console.log(numbers[i])
+}
